d1b3517e784a010685b12465500e0196a2c816fa
Leynse <timleynse94@gmail.com>
2023-05-03 13:47:09 +0200

    - Set heap flag in sfincs.vfproj to 0 to overcome stackoverflow error in netcdf input read. - Solution was checked and used in SVN rev 86 of 12-09-2019 already but had dissapeared by accident.
    - This setting should not influence model speed but still investigating not needing reshape-function could be interesting to avoid this.
     For this I tried quickly to add ', map()' to nf90_get_var, but then the order seemed wrong still (for which you would then again need 'reshape' function
     NF90(nf90_get_var(net_file_ampr%ncid, net_file_ampr%prcp_varid, prtmptest, start = (/ 1, 1, nt /), count = (/ ampr_ncols, ampr_nrows, 1 /), map = (/ 1, ampr_nrows, ampr_ncols /)))

--- source/src/sfincs_lib.f90
+++ source/src/sfincs_lib.f90
@@ -83,8 +83,8 @@ module sfincs_lib
    !
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !
-   build_revision = '$Rev: v2.0.1'
-   build_date     = '$Date: 2023-04-06$'
+   build_revision = '$Rev: v2.0.2_alpha'
+   build_date     = '$Date: 2023-05-03$'
    !
    write(*,'(a)')''   
    write(*,*)'----------- Welcome to SFINCS -----------'   
