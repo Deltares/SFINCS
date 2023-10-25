d90b2c494038d98bb00a090ad698c8a153287386
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2023-10-25 09:39:10 +0200

    Reordered and grouped BMI functions

--- source/src/sfincs_bmi.f90
+++ source/src/sfincs_bmi.f90
@@ -7,17 +7,21 @@
    implicit none
 
    public :: initialize
-   public :: update
    public :: finalize
+   public :: update
+   
+   public :: get_start_time
+   public :: get_end_time
+   public :: get_current_time
+   public :: get_time_step
+   
+   public :: get_var
    public :: get_var_shape
    public :: get_var_type
    public :: get_var_rank
+   
    public :: set_var
-   public :: get_start_time
-   public :: get_end_time
-   public :: get_time_step
-   public :: get_current_time
-
+   
    private
 
    integer(c_int), bind(C, name="maxstrlen") :: maxstrlen = 1024
@@ -41,18 +45,6 @@
    
 !-----------------------------------------------------------------------------------------------------!
    
-   function update(dt) result(ierr) bind(C, name="update")
-   !DEC$ ATTRIBUTES DLLEXPORT :: update
-   
-   real(kind=c_double), value, intent(in)  :: dt
-   integer(kind=c_int)              :: ierr
-   
-   ierr = sfincs_update(dt)
-   
-   end function update
-   
-!-----------------------------------------------------------------------------------------------------!
-   
    function finalize() result(ierr) bind(C, name="finalize")
    !DEC$ ATTRIBUTES DLLEXPORT :: finalize
    
@@ -64,44 +56,54 @@
    
 !-----------------------------------------------------------------------------------------------------!
    
-   subroutine get_var_shape(c_var_name, var_shape) bind(C, name="get_var_shape")
-   !DEC$ ATTRIBUTES DLLEXPORT :: get_var_shape
+   function update(dt) result(ierr) bind(C, name="update")
+   !DEC$ ATTRIBUTES DLLEXPORT :: update
    
-   character(kind=c_char), intent(in) :: c_var_name(*)
-   integer(c_int), intent(inout)      :: var_shape(maxdims)
-   character(len=strlen(c_var_name))  :: var_name
+   real(kind=c_double), value, intent(in)  :: dt
+   integer(kind=c_int)              :: ierr
+   
+   ierr = sfincs_update(dt)
+   
+   end function update
+   
+!-----------------------------------------------------------------------------------------------------!  
+      
+   subroutine get_start_time(tstart) bind(C, name="get_start_time")
+   !DEC$ ATTRIBUTES DLLEXPORT :: get_start_time
+   real(c_double), intent(out) :: tstart
 
-   var_name = char_array_to_string(c_var_name, strlen(c_var_name))
-   var_shape = (/0, 0, 0, 0, 0, 0/)
-   
-   select case(var_name)
-   case("xg", "yg","zs","zb","u","v")     
-      ! inverted shapes (fortran to c)
-      var_shape(2) = nmax
-      var_shape(1) = mmax
-   end select
-   
-   end subroutine get_var_shape
-   
-!-----------------------------------------------------------------------------------------------------!
-   
-   subroutine get_var_type(c_var_name, c_type) bind(C, name="get_var_type")
-   !DEC$ ATTRIBUTES DLLEXPORT :: get_var_type
-   
-   character(kind=c_char), intent(in)  :: c_var_name(*)
-   character(kind=c_char), intent(out) ::  c_type(maxstrlen)
-   character(len=maxstrlen)            :: type_name, var_name
+   tstart = t0
+   end subroutine get_start_time
 
-   var_name = char_array_to_string(c_var_name, strlen(c_var_name))
+!-----------------------------------------------------------------------------------------------------!  
+      
+   subroutine get_end_time(tend) bind(C, name="get_end_time")
+   !DEC$ ATTRIBUTES DLLEXPORT :: get_end_time
+   
+   real(c_double), intent(out) :: tend
 
-   select case(var_name)
-   case("xg", "yg", "zs", "zb", "u", "v")      
-      type_name = "float"  
-   end select
+   tend = t1
+   end subroutine get_end_time
    
-   c_type = string_to_char_array(trim(type_name), len(trim(type_name)))
+!-----------------------------------------------------------------------------------------------------!  
+      
+   subroutine get_current_time(tcurrent) bind(C, name="get_current_time")
+   !DEC$ ATTRIBUTES DLLEXPORT :: get_current_time
    
-   end subroutine get_var_type
+   real(c_double), intent(out) :: tcurrent
+
+   tcurrent = t
+   end subroutine get_current_time
+   
+!-----------------------------------------------------------------------------------------------------!  
+      
+   subroutine get_time_step(deltat) bind(C, name="get_time_step")
+   !DEC$ ATTRIBUTES DLLEXPORT :: get_time_step
+
+   real(c_double), intent(out) :: deltat
+
+   deltat = dt
+   end subroutine get_time_step
    
 !-----------------------------------------------------------------------------------------------------!
    
@@ -160,6 +162,47 @@
 
    end subroutine get_var
    
+   !-----------------------------------------------------------------------------------------------------!
+   
+   subroutine get_var_shape(c_var_name, var_shape) bind(C, name="get_var_shape")
+   !DEC$ ATTRIBUTES DLLEXPORT :: get_var_shape
+   
+   character(kind=c_char), intent(in) :: c_var_name(*)
+   integer(c_int), intent(inout)      :: var_shape(maxdims)
+   character(len=strlen(c_var_name))  :: var_name
+
+   var_name = char_array_to_string(c_var_name, strlen(c_var_name))
+   var_shape = (/0, 0, 0, 0, 0, 0/)
+   
+   select case(var_name)
+   case("xg", "yg","zs","zb","u","v")     
+      ! inverted shapes (fortran to c)
+      var_shape(2) = nmax
+      var_shape(1) = mmax
+   end select
+   
+   end subroutine get_var_shape
+   
+!-----------------------------------------------------------------------------------------------------!
+   
+   subroutine get_var_type(c_var_name, c_type) bind(C, name="get_var_type")
+   !DEC$ ATTRIBUTES DLLEXPORT :: get_var_type
+   
+   character(kind=c_char), intent(in)  :: c_var_name(*)
+   character(kind=c_char), intent(out) ::  c_type(maxstrlen)
+   character(len=maxstrlen)            :: type_name, var_name
+
+   var_name = char_array_to_string(c_var_name, strlen(c_var_name))
+
+   select case(var_name)
+   case("xg", "yg", "zs", "zb", "u", "v")      
+      type_name = "float"  
+   end select
+   
+   c_type = string_to_char_array(trim(type_name), len(trim(type_name)))
+   
+   end subroutine get_var_type
+   
 !-----------------------------------------------------------------------------------------------------!
    
    subroutine get_var_rank(c_var_name, rank) bind(C, name="get_var_rank")
@@ -217,45 +260,6 @@
    end select
          
    end subroutine set_var
-
-!-----------------------------------------------------------------------------------------------------!  
-      
-   subroutine get_start_time(tstart) bind(C, name="get_start_time")
-   !DEC$ ATTRIBUTES DLLEXPORT :: get_start_time
-   real(c_double), intent(out) :: tstart
-
-   tstart = t0
-   end subroutine get_start_time
-
-!-----------------------------------------------------------------------------------------------------!  
-      
-   subroutine get_end_time(tend) bind(C, name="get_end_time")
-   !DEC$ ATTRIBUTES DLLEXPORT :: get_end_time
-   
-   real(c_double), intent(out) :: tend
-
-   tend = t1
-   end subroutine get_end_time
-   
-!-----------------------------------------------------------------------------------------------------!  
-      
-   subroutine get_time_step(deltat) bind(C, name="get_time_step")
-   !DEC$ ATTRIBUTES DLLEXPORT :: get_time_step
-
-   real(c_double), intent(out) :: deltat
-
-   deltat = dt
-   end subroutine get_time_step
-   
-!-----------------------------------------------------------------------------------------------------!  
-      
-   subroutine get_current_time(tcurrent) bind(C, name="get_current_time")
-   !DEC$ ATTRIBUTES DLLEXPORT :: get_current_time
-   
-   real(c_double), intent(out) :: tcurrent
-
-   tcurrent = t
-   end subroutine get_current_time
    
 !-----------------------------------------------------------------------------------------------------!     
    ! private functions
@@ -270,7 +274,9 @@
       end if
    end do
    end function strlen
-!!!!!!!!!!!!!!!!!!!!!!!   
+   
+   !!!!!!!!!!!!!!!!!!!!!!!   
+   
    pure function char_array_to_string(char_array, length)
    integer(c_int), intent(in) :: length
    character(c_char),intent(in) :: char_array(length)
@@ -280,7 +286,9 @@
       char_array_to_string(i:i) = char_array(i)
    enddo
    end function char_array_to_string
-!!!!!!!!!!!!!!!!!!!!!!!      
+   
+   !!!!!!!!!!!!!!!!!!!!!!!      
+   
    pure function string_to_char_array(string, length)
    integer(c_int),intent(in) :: length
    character(len=length), intent(in) :: string
