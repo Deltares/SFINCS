9bcea1768747d8209fabd21e7f3eeabdb13710ba
Leynse <timleynse94@gmail.com>
2024-02-14 17:22:46 +0100

    - Change that check if sbgfile is netcdf or not is not done using netcdf library (causing in weird error message for user), but do a more clear check on the extension being '.nc' or not. - Therefore error message is adapted

--- source/src/sfincs_subgrid.F90
+++ source/src/sfincs_subgrid.F90
@@ -21,13 +21,17 @@ contains
    !
    implicit none
    !
+   integer       :: j
+   character*2   :: ext
+   character*256 :: tmpfile
+   !   
    ! Check what sort of file we're dealing with
    !
-   NF90(nf90_open(trim(sbgfile), NF90_CLOBBER, net_file_sbg%ncid))
-   !
-   if (net_file_sbg%ncid > 0) then
-      !
-      NF90(nf90_close(net_file_sbg%ncid))
+   tmpfile = trim(sbgfile)
+   j=index(tmpfile,'.')
+   ext = tmpfile(j+1:j+2)
+   
+   if (ext == 'nc') then
       !
       ! Netcdf format with havg and nrep
       ! 
@@ -39,7 +43,7 @@ contains
       !
       ! Binary format with hrep and navg
       !
-      write(*,*)'Above error appears because your subgrid file has the "old" binary format. Nothing to worry about.'
+      write(*,*)'Warning : subgrid file has the "old" binary format, the simulation will continue, but we do recommended switching to the new Netcdf subgrid input format!'      
       ! 
       call read_subgrid_file_original()
       !
