bf60f71740997b27ee4c6f6965d0fa0346778649
maartenvanormondt <maarten.vanormondt@deltares.nl>
2024-05-31 14:17:01 -0400

    removed mybuild

--- source/third_party_open/netcdf/MyBuild/CMakeFiles/IntelVSImplicitPath/output.cmake
+++
@@ -1,1 +0,0 @@
-list(APPEND implicit_dirs "C:/Program Files (x86)/Intel/oneAPI/compiler/2022.2.0/windows/compiler/lib/Intel64_win")
--- source/third_party_open/netcdf/MyBuild/examples/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/work/netcdf/netcdf-fortran-main-4.6.1/netcdf-fortran-main/examples
-# Build directory: C:/work/netcdf/netcdf-fortran-main-4.6.1/MyBuild/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("F77")
-subdirs("F90")
