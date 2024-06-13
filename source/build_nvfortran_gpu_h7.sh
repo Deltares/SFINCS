d72fe56ff3b6b73a7f38c916441d84a84578b4a5
maartenvanormondt <82946105+maartenvanormondt@users.noreply.github.com>
2024-06-13 13:33:48 -0400

    update build nvidia h7

--- source/build_nvfortran_gpu_h7.sh
+++ source/build_nvfortran_gpu_h7.sh
@@ -10,17 +10,9 @@ find . -name \*.f90|xargs dos2unix
 find . -name \*.F90|xargs dos2unix
 find . -name \*.am|xargs dos2unix
 
-#MANPATH=$MANPATH:/opt/nvidia/hpc_sdk/Linux_x86_64/24.5/compilers/man; export MANPATH
-#PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/24.5/compilers/bin:$PATH; export PATH
-
-#apt install -y build-essential autoconf automake libtool pkg-config tzdata
-#export CONFIG_SHELL=/bin/bash
-
-#autoreconf -vif
-
 ./autogen.sh
 
-./configure FCFLAGS="-acc -Minfo=accel -fast -O3 -gpu=ccall -DSIZEOF_PTRDIFF_T=999" FC=nvfortran --disable-shared --disable-openmp --prefix bin/sfincs
+./configure FCFLAGS="-acc -Minfo=accel -fast -O3 -gpu=ccall -DSIZEOF_PTRDIFF_T=999" FC=nvfortran --disable-shared --disable-openmp --prefix /u/${USER}/bin/sfincs_nvfortran_gpu
 
 make clean
 
