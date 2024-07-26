f6698cd0b1ff9d91148dfec7057f81474f3a50ef
Leynse <timleynse94@gmail.com>
2024-07-26 11:01:08 +0200

    - Turn warnings back on

--- source/src/snapwave/snapwave_boundaries.f90
+++ source/src/snapwave/snapwave_boundaries.f90
@@ -540,14 +540,14 @@ subroutine update_boundary_points(t)
       ! Limit directional spreading (2 < ds < 45)
       !       
       if ((dsp < 2*pi/180) .or.(dsp > 45*pi/180)) then  
-!      	  write(*,*)'DEBUG SnapWave - input wave spreading is outside acceptable range of 2-45 degrees: ',dsp/pi*180, ' and is therefore limited back to this range, please check whether input is realistic!'          
+      	  write(*,*)'DEBUG SnapWave - input wave spreading is outside acceptable range of 2-45 degrees: ',dsp/pi*180, ' and is therefore limited back to this range, please check whether input is realistic!'          
           dsp = max(min(dsp, 45*pi/180), 2*pi/180)
       endif      
       !
       ! Limit period (2 < tps < 25)
       !       
       if ((tps < 2.0) .or.(tps > 25.0)) then
-!      	  write(*,*)'DEBUG SnapWave - input wave period is outside acceptable range of 2-25 s: ',tps, ' and is therefore limited back to this range, please check whether input is realistic!'
+      	  write(*,*)'DEBUG SnapWave - input wave period is outside acceptable range of 2-25 s: ',tps, ' and is therefore limited back to this range, please check whether input is realistic!'
           tps = max(min(tps, 25.0), 2.0)
       endif      
       !
