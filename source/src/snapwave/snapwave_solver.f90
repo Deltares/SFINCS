463f19ee09664399935813f2c9b1b3a6159d5b57
Tim Leijnse <timleynse94@gmail.com>
2024-11-21 09:45:34 +0100

    115 add implicit ig source option in snapwave (#124)
    * - We don't actually use snapwave_results.f90 and snapwave_ncoutput.F90, so removed
    
    * - Make limits in check spreading and period input a bit more lenient
    
    * - Add logo and SnapWave build revision (read: snapwave_solver.f90 version)
    
    * - Remove last references to snapwave_ncoutput.f90 and _results.f90
    - Add snapwave_windsource.f90 from SnapWave repo
    
    * - Copy snapwave_solver.f90 one-on-one
    
    * - Make updated snapwave_solver compilable
    - now is simply 'alpha' and 'gamma' for snapwave, no overlap with SFINCS variables
    - remove 'time' and 'ja_save_each_iter' loop in snapwave_solver
    
    * - change ja_vegetation into vegetation_opt and vegetation (as logical)
    - use igwaves, wind and vegetation as logical in snapwave_solver
    - Add new paras of 'niter' and 'Tpini'
    - Only show igherbers message now if igwaves=1
    
    * - Make hmin, baldock_ratio and baldock_ratio_ig user definable again
    - Use Tp_ig as determined in snapwave_boundaries again
    
    * - Put back the output and calculation of betamean, alphaig, srcig, Df_ig, Dw_ig
    
    * - Add sector option as in SnapWave trunk
    
    * - Important fix that the Fx&Fy should be multiplied again with rho*depth(k)! - Reason; unit was different in our old version
    - Note also that it is now 'F(k) = Dw(k)*...' instead of F(k) = (Dw(k) + Df(k))*...
    - Results for Duck and Wrightsville Beach overall are very similar, though differences in short wave height H and Fx&Fy near the waterline do exist (bit more dissipation in new version, must be related to snapwave_solver underlying changes)
    
    * - NOTE! in Baldock formula is now 0.28*alfa instead of 0.25*alfa!
    
    * - First step in implementation of parsing windu and windv from SFINCS to SnapWave as u10 and u10dir after conversion to magnitude and direction
    - So far only working is storemeteo=1 (otherwise SFINCS only keeps the tauw, not also the wind speeds
    - So far only works for spatially varying wind fields (meteo3d), so not uniform wndfile
    
    * - Added conversion wind direction to radians
    - Added wind forcing in snapwave_boundaries, including subroutines 'update_wind_field' (simplified), 'make_theta_grid' and 'build_boundary_support_points_spectra' (to be used later)
    - Change default value of 'crit' to 0.00001 instead of 0.01 as is current default in SnapWave trunk
    - Performed succesful principal test adding wind to Duck model and seeing wind growth, no validation yet
    
    * - Comment on old defaults
    
    * - Set directional spreading check higher to 3 degrees because for value of 1-2 the Herbers BC estimation might give values of NaN for IG wave height and Tp
    
    * - Remove setting ack to 5m for now while testing the GLOBEX runs
    
    * - add logical 'store_wind' to indicate that we have to keep 'windu'and 'windv' variables. Either to store in netcdf output, or give to SnapWave in sfincs_snapwave.f90 (if snapwave_wind = 1, by default off)
    - therefore at some places 'store_meteo' has been swapped for 'store_wind'
    - Also, now also uniform wind from wndfile can be parsed to SnapWave
    - Important bugfix in directional conversion in sfincs_snapwave.f90! Well tested for wndfile directions
    
    * - Version including implicit IG source by putting 'determine_infragravity_source_sink_term' in the iter-loop for sweep=1
    
    * - We need to update some variables every time as well then
    
    * -  Cgprev, Eeprev, eeprev_ig and Hprev are determined within subroutine > so not needed to update beforehand actually!
    
    * - Make user parameter to turn on implicit behaviour (iterative_srcig=1, default) or off (iterative_srcig=0) for IG source term
    - Combine update of E(k) and H(k) in one
    
    * - Remove comment

--- source/src/snapwave/snapwave_solver.f90
+++ source/src/snapwave/snapwave_solver.f90
@@ -138,7 +138,7 @@ module snapwave_solver
                                          igwaves,kwav_ig, cg_ig,H_ig,ctheta_ig,Hmx_ig, ee_ig,fw_ig, &
                                          beta, srcig, alphaig, Dw_ig, Df_ig, &
                                          vegetation, no_secveg, veg_ah, veg_bstems, veg_Nstems, veg_Cd, Dveg, &
-                                         zb, nwav, ig_opt, alpha_ig, gamma_ig, eeinc2ig, Tinc2ig, alphaigfac, shinc2ig)
+                                         zb, nwav, ig_opt, alpha_ig, gamma_ig, eeinc2ig, Tinc2ig, alphaigfac, shinc2ig, iterative_srcig)
       !
       call timer(t3)
       !
@@ -165,7 +165,7 @@ module snapwave_solver
                                          igwaves,kwav_ig, cg_ig,H_ig,ctheta_ig,Hmx_ig, ee_ig,fw_ig, &
                                          betamean, srcig, alphaig, Dw_ig, Df_ig, &       
                                          vegetation, no_secveg, veg_ah, veg_bstems, veg_Nstems, veg_Cd, Dveg, &
-                                         zb, nwav, ig_opt, alfa_ig, gamma_ig, eeinc2ig, Tinc2ig, alphaigfac, shinc2ig)
+                                         zb, nwav, ig_opt, alfa_ig, gamma_ig, eeinc2ig, Tinc2ig, alphaigfac, shinc2ig, iterative_srcig)
    !
    use snapwave_windsource
    !use snapwave_ncoutput ! TL: removed, we don't use this in SF+SW
@@ -185,6 +185,7 @@ module snapwave_solver
    logical, dimension(no_nodes), intent(inout)      :: inner                  ! mask of inner grid points (not on boundary)
    logical, intent(in)                              :: wind                   ! logical wind on/off
    logical, intent(in)                              :: igwaves                ! logical IG waves on/off
+   integer, intent(in)                              :: iterative_srcig        ! option whether IG source/sink term should be calculated implicitly (iterative_srcig = 1), or just a priori based on effectively incident wave energy from previous timestep only (iterative_srcig=0, explicitly, bit faster)
    integer, dimension(no_nodes),intent(in)          :: neumannconnected       ! number of neumann boundary point if connected to inner point
    real*4, dimension(no_nodes), intent(in)          :: depth                  ! water depth
    real*4, dimension(no_nodes), intent(in)          :: zb                     ! actual bed level
@@ -527,6 +528,29 @@ module snapwave_solver
          do k = 1, no_nodes
             Eold(k) = sum(eeold(:, k))
          enddo
+         !
+         if (igwaves) then
+            !        
+            if (iterative_srcig==1) then 
+                ! Update H(k) based on updated ee(:,k), as used in IG source term to determine alphaig
+                ! 
+                do k = 1, no_nodes   
+                   ! 
+                   if (inner(k)) then             
+                       !
+                       H(k)      = sqrt(8*sum(ee(:, k))*dtheta/rho/g)                   
+                       !
+                   endif               
+                enddo
+                !
+                ! Actual determining of source term - every first sweep of iteration
+                !          
+                call determine_infragravity_source_sink_term(inner, no_nodes, ntheta, w, ds, prev, cg_ig, nwav, depth, zb, H, ee, ee_ig, eeprev, eeprev_ig, cgprev, ig_opt, alphaigfac, alphaig_local, beta_local, srcig_local) 
+                !    
+            endif
+            !
+         endif   
+         !
       endif
       !
       !
@@ -821,8 +845,6 @@ module snapwave_solver
          endif
          !
       enddo
-      !
-      ! TODO - TL: Check whether can put back here the srcig_iterative == 1
       !
       if (sweep==4) then
          !
