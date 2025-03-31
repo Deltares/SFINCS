f9e13337a167e328df56412c9da8c9daa2dbc0bf
Leynse <Tim.Leijnse@deltares.nl>
2025-03-31 14:53:14 +0200

    - Set 'min_dt' to small value instead of 0, since that caused our drain discharge to because NaN because of dividing through 0 - Note, this is the same commit as Maarten did in https://github.com/Deltares/SFINCS/pull/140

--- source/src/sfincs_lib.f90
+++ source/src/sfincs_lib.f90
@@ -240,10 +240,10 @@ module sfincs_lib
    t           = t0     ! start time
    tout        = t0
    dt          = 1.0e-6 ! First time step very small
+   min_dt      = 1.0e-6 ! First time step very small
    dtavg       = 0.0    ! average time step
    maxdepth    = 999.0  ! maximum depth over time step
    maxmaxdepth = 0.0    ! maximum depth over entire simulation
-   min_dt      = 0.0    ! minimum time step from compute_fluxes
    nt          = 0      ! number of time steps
    ntmapout    = 0      ! number of map time steps
    ntmaxout    = 0      ! number of max time steps
