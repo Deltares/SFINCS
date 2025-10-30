fb5ca13d625073fa1870a7d426dbe7a718e44d8c
Tim Leijnse <timleynse94@gmail.com>
2025-10-30 17:26:08 +0100

    - Add infiltration to logfile processes section (#221)

--- source/src/sfincs_lib.f90
+++ source/src/sfincs_lib.f90
@@ -92,8 +92,8 @@ module sfincs_lib
    !
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !
-   build_revision = "$Rev: v2.3.0-alpha mt.Faber-branch:218"
-   build_date     = "$Date: 2025-10-17"
+   build_revision = "$Rev: v2.3.0-alpha mt.Faber"
+   build_date     = "$Date: 2025-10-30"
    !
    call write_log('', 1)
    call write_log('------------ Welcome to SFINCS ------------', 1)
@@ -220,6 +220,11 @@ module sfincs_lib
    else   
       call write_log('Precipitation        : no', 1)
    endif
+   if (infiltration) then
+      call write_log('Infiltration         : yes', 1)
+   else   
+      call write_log('Infiltration         : no', 1)
+   endif   
    if (snapwave) then
       call write_log('SnapWave             : yes', 1)
    else
