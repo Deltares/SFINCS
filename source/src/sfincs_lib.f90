Deltares/SFINCS

commit f964699f24f46aae5af22e5d797149c86e34e058

Bathtub (#295)
* added bathtub approach

* added some variables

* added interp_segment

* - Tmp bump version

* - Fix because Maarten changed default from '' to none

* alfa not needed

* Keep defaults of snapwave_eeinc2ig and T2inc as they were

---------

Co-authored-by: Leynse <Tim.Leijnse@deltares.nl>
Co-authored-by: Tim Leijnse <timleynse94@gmail.com>

source/src/sfincs_lib.f90
@@ -23,6 +23,7 @@ module sfincs_lib
    use sfincs_snapwave
    use sfincs_wavemaker
    use sfincs_nonhydrostatic
+   use sfincs_bathtub
    use sfincs_openacc
    use sfincs_log
    !
@@ -140,8 +141,13 @@ module sfincs_lib
    call write_log('Reading input file ...', 0) 
    call read_sfincs_input()     ! Reads sfincs.inp
    !
-   call write_log('Reading meteo data ...', 0) 
-   call read_meteo_data()       ! Reads meteo data (amu, amv, spw file etc.)
+   if (.not. bathtub) then
+      !
+      call write_log('Reading meteo data ...', 0)
+      !
+      call read_meteo_data()       ! Reads meteo data (amu, amv, spw file etc.)
+      !
+   endif   
    !
    call write_log('Preparing domain ...', 0) 
    call initialize_domain()     ! Reads dep, msk, index files, creates index, flag and depth arrays, initializes hydro quantities
@@ -175,6 +181,14 @@ module sfincs_lib
       call read_wavemaker_polylines()
       !
    endif
+   !
+   if (bathtub) then
+      !
+      call write_log('Initialize bathtub mode ...', 0) 
+      !
+      call initialize_bathtub()
+      !
+   endif   
    !
    call write_log('', 1)   
    call write_log('------------------------------------------', 1)
@@ -239,6 +253,11 @@ module sfincs_lib
       call write_log('Non-hydrostatic      : yes', 1)
    else
       ! call write_log('Non-hydrostatic         : no', 1)
+   endif   
+   if (bathtub) then
+      call write_log('Bathtub              : yes', 1)
+   else
+      ! call write_log('Bathtub              : no', 1)
    endif   
    call write_log('------------------------------------------', 1) 
    call write_log('', 1)   
@@ -324,7 +343,7 @@ module sfincs_lib
    integer                       :: ierr
    real*8                        :: tend !< end of update interval
    real*4                        :: dtchk !< dt to check for instability
-   logical                       :: single_time_step 
+   logical                       :: single_time_step
    !
    ierr = 0
    !
@@ -374,6 +393,19 @@ module sfincs_lib
       nt = nt + 1
       dt = alfa * min_dt ! min_dt was computed in sfincs_momentum.f90 without alfa
       dtchk = alfa * min_dt
+      !
+      if (bathtub) then
+         !
+         ! In bathtub mode, use fixed time step
+         !
+         if (nt > 1) then
+            !
+            dt = bathtub_dt
+            dtchk = bathtub_dt
+            !
+         endif   
+         !
+      endif
       !
       ! A bit unclear why this happens, but large jumps in the time step lead to weird oscillations.
       ! In the 'original' sfincs v11 version, this behavior was supressed by the use of theta.
@@ -438,7 +470,7 @@ module sfincs_lib
       !
       ! Check whether history output is required at this time step
       !
-      if (t>=thisout) then
+      if (t >= thisout) then
          !
          write_his = .true.
          nthisout  = nthisout + 1
@@ -447,7 +479,7 @@ module sfincs_lib
          !
       endif
       !
-      if (debug .and. t>=t0out) then
+      if (debug .and. t >= t0out) then
          !
          ! Write every time step to map and his file when in debug mode
          !
@@ -540,39 +572,49 @@ module sfincs_lib
          !
       endif   
       !
-      ! And now for the real computations !
-      !
-      ! First compute fluxes
-      !
-      call compute_fluxes(dt, tloopflux)
-      !
-      if (wavemaker) then
+      if (bathtub) then
          !
-         call update_wavemaker_fluxes(t, dt, tloopwavemaker)
-         !         
-      endif   
-      !
-      if (nrstructures>0) then
+         ! In bathtub mode, only update water levels based on boundary conditions
          !
-         call compute_fluxes_over_structures(tloopstruc)
+         call bathtub_compute_water_levels(tloopcont)
          !
-      endif
-      !      
-      if (nonhydrostatic) then
+      else
          !
-         if (t < nh_tstop) then ! Check if non-hydrostatic corrections still need to be made
+         ! And now for the real computations ! Unless we're in bathtub mode
+         !
+         ! First compute fluxes
+         !
+         call compute_fluxes(dt, tloopflux)
+         !
+         if (wavemaker) then
             !
-            ! Apply non-hydrostatic pressure corrections to q and uv
-            !
-            call compute_nonhydrostatic(dt, tloopnonh)
-            !
+            call update_wavemaker_fluxes(t, dt, tloopwavemaker)
+            !         
          endif   
          !
-      endif
-      !      
-      ! Update water levels
-      !
-      call compute_water_levels(t, dt, tloopcont)
+         if (nrstructures>0) then
+            !
+            call compute_fluxes_over_structures(tloopstruc)
+            !
+         endif
+         !      
+         if (nonhydrostatic) then
+            !
+            if (t < nh_tstop) then ! Check if non-hydrostatic corrections still need to be made
+               !
+               ! Apply non-hydrostatic pressure corrections to q and uv
+               !
+               call compute_nonhydrostatic(dt, tloopnonh)
+               !
+            endif   
+            !
+         endif
+         !      
+         ! Update water levels
+         !
+         call compute_water_levels(t, dt, tloopcont)
+         !
+      endif   
       !
       ! OUTPUT
       !      

source/sfincs_lib/sfincs_lib.vfproj
@@ -31,7 +31,8 @@
 		<Filter Name="Source Files" Filter="f90;for;f;fpp;ftn;def;odl;idl">
 			<Filter Name="snapwave">
 				<File RelativePath="..\src\snapwave\interp.F90"/>
-				<File RelativePath="..\src\snapwave\snapwave_boundaries.f90"/>
+				<File RelativePath="..\src\snapwave\snapwave_boundaries.f90">
+				</File>
 				<File RelativePath="..\src\snapwave\snapwave_data.f90"/>
 				<File RelativePath="..\src\snapwave\snapwave_date.f90"/>
 				<File RelativePath="..\src\snapwave\snapwave_domain.f90"/>
@@ -51,17 +52,21 @@
 			<File RelativePath="..\third_party_open\bicgstab\bicgstab_solver_ilu.f90"/>
 			<File RelativePath="..\third_party_open\utils\deg2utm.f90"/>
 			<File RelativePath="..\third_party_open\utils\geometry.f90"/>
+			<File RelativePath="..\src\sfincs_bathtub.f90">
+			</File>
 			<File RelativePath="..\src\sfincs_boundaries.f90"/>
 			<File RelativePath="..\src\sfincs_continuity.f90"/>
 			<File RelativePath="..\src\sfincs_crosssections.f90"/>
-			<File RelativePath="..\src\sfincs_data.f90"/>
+			<File RelativePath="..\src\sfincs_data.f90">
+			</File>
 			<File RelativePath="..\src\sfincs_date.f90"/>
 			<File RelativePath="..\src\sfincs_discharges.f90"/>
 			<File RelativePath="..\src\sfincs_domain.f90"/>
 			<File RelativePath="..\src\sfincs_error.f90"/>
 			<File RelativePath="..\src\sfincs_infiltration.f90"/>
 			<File RelativePath="..\src\sfincs_initial_conditions.F90"/>
-			<File RelativePath="..\src\sfincs_input.f90"/>
+			<File RelativePath="..\src\sfincs_input.f90">
+			</File>
 			<File RelativePath="..\src\sfincs_lib.f90"/>
 			<File RelativePath="..\src\sfincs_log.f90"/>
 			<File RelativePath="..\src\sfincs_meteo.f90"/>

source/src/Makefile.am
@@ -55,6 +55,7 @@ libsfincs_la_SOURCES = \
 	sfincs_momentum.f90 \
 	sfincs_wavemaker.f90 \
 	sfincs_infiltration.f90 \
+	sfincs_bathtub.f90 \
 	sfincs_openacc.f90 \
 	sfincs_lib.f90 \
 	sfincs_bmi.f90

source/src/sfincs_bathtub.f90
@@ -1,0 +1,216 @@
+module sfincs_bathtub
+
+integer*4, dimension(:),   allocatable :: bathtub_i1
+integer*4, dimension(:),   allocatable :: bathtub_i2
+real*4,    dimension(:),   allocatable :: bathtub_w1
+integer*4                              :: bathtub_snapwave_nwbnd
+real*4, allocatable  :: bathtub_snapwave_x_bwv(:)
+real*4, allocatable  :: bathtub_snapwave_y_bwv(:)
+real*4, allocatable  :: bathtub_snapwave_hs_bwv(:)
+integer, allocatable :: bathtub_snapwave_i1(:)
+integer, allocatable :: bathtub_snapwave_i2(:)
+real*4, allocatable  :: bathtub_snapwave_w1(:)
+
+contains
+   !
+   subroutine initialize_bathtub()
+   !
+   ! Creates bathtub boundary conditions and determines weighting factors
+   !
+   use sfincs_data
+   use geometry
+   use quadtree
+   use sfincs_error
+   use sfincs_log
+   !
+   implicit none
+   !
+   integer   :: ip, ib, itb
+   integer   :: i1, i2
+   real*4    :: w1, w2
+   real*4    :: h1, h2
+   real*8    :: t8
+   !
+   if (bathtub_snapwave) then
+      !
+      ! First read wave boundary conditions. Same as in snapwave.
+      ! We cannot use snapwave_data and snapwave_boundaries unfortunately, as these share common variable names.
+      !
+      call read_snapwave_boundary_data() ! snapwave_boundaries
+      !
+      ! Determine weights and indices for each wave boundary point
+      !
+      allocate(bathtub_snapwave_i1(nbnd))
+      allocate(bathtub_snapwave_i2(nbnd))
+      allocate(bathtub_snapwave_w1(nbnd))
+      !
+      ! Loop along boundary points
+      !
+      do ib = 1, nbnd
+         !
+         call interp_segment(bathtub_snapwave_x_bwv, bathtub_snapwave_y_bwv, bathtub_snapwave_nwbnd, x_bnd(ib), y_bnd(ib), i1, i2, w1, w2)
+         !
+         bathtub_snapwave_i1(ib) = i1
+         bathtub_snapwave_i2(ib) = i2
+         bathtub_snapwave_w1(ib) = w1      
+         !
+      enddo
+      !
+      ! Loop through times in bzs file and add f*Hm0 (typically f=0.2)
+      !
+      do itb = 1, ntbnd
+         !
+         ! Need to convert t_bnd to real*8
+         !
+         t8 = t_bnd(itb)
+         !
+         call update_snapwave_boundary_data(t8)
+         !
+         ! Loop through boundary points
+         !
+         do ib = 1, nbnd            
+            !
+            h1 = bathtub_snapwave_hs_bwv(bathtub_snapwave_i1(ib))
+            h2 = bathtub_snapwave_hs_bwv(bathtub_snapwave_i2(ib))
+            w1 = bathtub_snapwave_w1(ib)
+            w2 = 1.0 - w1
+            !
+            zs_bnd(ib, itb) = zs_bnd(ib, itb) + bathtub_fac_hs * (w1 * h1 + w2 * h2)
+            !
+         enddo   
+         !
+      enddo   
+      !
+   endif   
+   !
+   ! Determine indices and weight for each grid point
+   !
+   allocate(bathtub_i1(np))
+   allocate(bathtub_i2(np))
+   allocate(bathtub_w1(np))
+   !
+   do ip = 1, np
+      !
+      call interp_segment(x_bnd, y_bnd, nbnd, z_xz(ip), z_yz(ip), i1, i2, w1, w2)
+      !
+      bathtub_i1(ip) = i1
+      bathtub_i2(ip) = i2
+      bathtub_w1(ip) = w1
+      !
+   enddo
+   !
+   meteo3d = .false.
+   wind = .false.
+   store_meteo = .false.
+   store_wind = .false.
+   store_wind_max = .false.
+   precip = .false.
+   patmos = .false.
+   snapwave = .false.
+   infiltration = .false.
+   store_velocity = .false.
+   store_maximum_velocity = .false.
+   !
+   end subroutine
+
+   
+   subroutine bathtub_compute_water_levels(tloop)
+   !
+   use sfincs_data
+   use geometry
+   !
+   implicit none
+   !
+   integer  :: count0
+   integer  :: count1
+   integer  :: count_rate
+   integer  :: count_max
+   real     :: tloop
+   !
+   integer :: nm, i1, i2
+   real*4  :: zbt, w1, w2
+   !
+   call system_clock(count0, count_rate, count_max)
+   !
+   !$omp parallel &
+   !$omp private ( nm, i1, i2, w1, w2 )
+   !$omp do schedule ( dynamic, 256 )
+   do nm = 1, np
+      !
+      i1 = bathtub_i1(nm)
+      i2 = bathtub_i2(nm)
+      w1 = bathtub_w1(nm)
+      w2 = 1.0 - w1
+      !
+      zbt = w1 * zst_bnd(i1) + w2 * zst_bnd(i2)
+      !
+      if (subgrid) then
+         !
+         zs(nm) = max(subgrid_z_zmin(nm), zbt)
+         !
+      else
+         !
+         zs(nm) = max(zb(nm), zbt)
+         !
+      endif
+      !
+      if (store_maximum_waterlevel) then
+         !
+         ! Store the maximum water level
+         !
+         zsmax(nm) = max(zsmax(nm), zs(nm))
+         !
+      endif      
+      !
+   enddo   
+   !$omp end do
+   !$omp end parallel
+   !
+   !$acc update device( zs, zsmax )
+   !
+   call system_clock(count1, count_rate, count_max)
+   tloop = tloop + 1.0 * (count1 - count0) / count_rate
+   !
+   end subroutine
+
+   subroutine read_snapwave_boundary_data()
+   !
+   use snapwave_data
+   use snapwave_boundaries
+   use sfincs_snapwave
+   !
+   ! Read SnapWave input file from sfincs.inp (this will store boundary file names in snapwave_data)
+   !
+   call read_snapwave_input()
+   !
+   ! Use read_boundary data from snapwave_boundaries
+   !
+   call read_boundary_data()   
+   !
+   allocate(bathtub_snapwave_x_bwv(nwbnd))
+   allocate(bathtub_snapwave_y_bwv(nwbnd))
+   allocate(bathtub_snapwave_hs_bwv(nwbnd))
+   !
+   bathtub_snapwave_nwbnd = nwbnd
+   bathtub_snapwave_x_bwv = x_bwv
+   bathtub_snapwave_y_bwv = y_bwv
+   !
+   end subroutine
+
+   
+   subroutine update_snapwave_boundary_data(tb)
+   !
+   use snapwave_data
+   use snapwave_boundaries   
+   !
+   implicit none
+   !
+   real*8 :: tb
+   !
+   call update_boundary_points(tb, .true.)
+   !
+   bathtub_snapwave_hs_bwv = hst_bwv
+   !
+   end subroutine
+   
+end module

source/src/sfincs_boundaries.f90
@@ -401,7 +401,7 @@ contains
          !
          if (ios /= 0) exit
          !
-         if (trim(line) == '[forcing]') then
+         if (trim(line) == '[forcing]' .or. trim(line) == '[Forcing]') then
             !
             current_set = current_set + 1
             current_component = 0
@@ -448,10 +448,6 @@ contains
       call update_nodal_factors(i_date_time, tidal_component_names, nr_tidal_components, nbnd, tidal_component_data, tidal_component_frequency)
       !
       tidal_component_frequency = tidal_component_frequency / 3600 ! Convert to rad/s
-      !
-      !do ios = 1, nr_tidal_components
-      !   write(*,'(a,20f16.3)')tidal_component_names(ios), (180.0 / pi) * tidal_component_frequency(ios) * 3600.0,tidal_component_data(1,ios,1), (180.0 / pi) * tidal_component_data(2,ios,1)
-      !enddo   
       !
    endif      
    !
@@ -1128,13 +1124,20 @@ contains
          !
       endif
       !
-      ! Update boundary conditions at grid points (water levels)
+      ! In case of bathtub, we do not need to update boundary conditions at grid points or boundary fluxes,
+      ! as these are not used in bathtub mode
       !
-      call update_boundary_conditions(t, dt)
-      !
-      ! Update boundary fluxes
-      !
-      call update_boundary_fluxes(dt, t)
+      if (.not. bathtub) then
+         !      
+         ! Update boundary conditions at grid points (water levels)
+         !
+         call update_boundary_conditions(t, dt)
+         !
+         ! Update boundary fluxes
+         !
+         call update_boundary_fluxes(dt, t)
+         !
+      endif
       !
    endif
    !

source/src/sfincs_data.f90
@@ -262,7 +262,9 @@ module sfincs_data
       logical       :: h73table
       logical       :: wave_enhanced_roughness
       logical       :: use_bcafile
-      LOGICAL       :: snapwave_use_nearest
+      logical       :: snapwave_use_nearest
+      logical       :: bathtub
+      logical       :: bathtub_snapwave      
       !!!
       !!! sfincs_input.f90 switches
       integer storevelmax
@@ -788,6 +790,9 @@ module sfincs_data
       integer, dimension(:),     allocatable   :: runup_gauge_nrp
       !
       real*4 :: waveage
+      !
+      real*4 :: bathtub_dt
+      real*4 :: bathtub_fac_hs
       !
       ! LGX Cd table
       !

source/src/sfincs_input.f90
@@ -144,6 +144,9 @@ contains
    call read_real_input(500, 'factor_pres', factor_pres, 1.0)
    call read_real_input(500, 'factor_prcp', factor_prcp, 1.0)
    call read_real_input(500, 'factor_spw_size', factor_spw_size, 1.0)   
+   call read_logical_input(500, 'bathtub', bathtub, .false.)
+   call read_real_input(500, 'bathtub_fachs', bathtub_fac_hs, 0.2)
+   call read_real_input(500, 'bathtub_dt', bathtub_dt, -999.0)
    !
    ! Domain
    !
@@ -629,6 +632,44 @@ contains
          nh_tstop = t1 + 999.0
          !          
       endif    
+      !
+   endif
+   !
+   if (bathtub) then
+      !
+      call write_log('Info    : turning on process: Bathtub flooding', 0)
+      !
+      ! Set time step
+      !
+      if (bathtub_dt < 0.0) then
+         !
+         ! Time step for simulation not defined so use same as map output
+         !
+         bathtub_dt = dtmapout
+         !
+      endif
+      !
+      dthisout = bathtub_dt
+      !
+      ! Turn off some processes not needed for bathtub flooding
+      !
+      nsrc = 0
+      ndrn = 0
+      !
+      meteo3d = .false.
+      wind = .false.
+      store_meteo = .false.
+      store_wind = .false.
+      store_wind_max = .false.
+      precip = .false.
+      patmos = .false.
+      if (snapwave) then
+         bathtub_snapwave = .true.
+      endif
+      snapwave = .false.
+      infiltration = .false.
+      store_velocity = .false.
+      store_maximum_velocity = .false.
       !
    endif
    !

commit 4ecb793ee0a4482d25c535c3fb7d6184395854a5

- Fix issue for twice calling of find_boundary_indices for Snapwave, as introduced in PR #295 - Only became apparent for testbed model Outerbanks_quadtree_SFINCS_SnapWave_IG - Keep the one in snapwave_boundaries.f90' subroutine couple_read_boundary_data - Result for bathtub_snapwave testbed model is unchanged

source/src/sfincs_lib.f90
@@ -95,7 +95,7 @@ module sfincs_lib
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !
    build_revision = "$Rev: v2.3.2 mt. Faber+"
-   build_date     = "$Date: 2026-03-19"
+   build_date     = "$Date: 2026-03-20"
    !
    call write_log('', 1)
    call write_log('------------ Welcome to SFINCS ------------', 1)

source/src/sfincs_snapwave.f90
@@ -105,7 +105,7 @@ contains
    !
    call read_boundary_data()
    !
-   call find_boundary_indices()
+   !call find_boundary_indices() ! > is already called in read_boundary_data()
    !
    call write_log('', 1)
    !