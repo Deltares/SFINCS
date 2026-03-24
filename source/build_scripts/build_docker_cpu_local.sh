88a7e5b4bd34785c8e90f0e9510971ecf2d76286
Leynse <Tim.Leijnse@deltares.nl>
2026-03-24 17:30:10 +0100

    - m4 & autogen back

--- source/build_scripts/build_docker_cpu_local.sh
+++ source/build_scripts/build_docker_cpu_local.sh
@@ -1,4 +1,4 @@
 cd /mnt/c/users/leijnse/repos/SFINCS/source
 
-docker build -f ./build_scripts/Dockerfile_cpu_local . -t leynse/sfincs-cpu > build.log 2>&1
+docker build -f ./build_scripts/Dockerfile . -t leynse/sfincs-cpu > build.log 2>&1
 docker push leynse/sfincs-cpu
