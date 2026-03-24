4d3df7a3f2fd5d8befadd0e398a3c9c66c523309
Leynse <Tim.Leijnse@deltares.nl>
2026-03-24 16:49:25 +0100

    - Need configure and Makefile one level higher - Move m4 folder into build_scripts

--- source/build_scripts/build_docker_cpu_local.sh
+++ source/build_scripts/build_docker_cpu_local.sh
@@ -1,4 +1,4 @@
 cd /mnt/c/users/leijnse/repos/SFINCS/source
 
-docker build -f Dockerfile_cpu_local . -t leynse/sfincs-cpu
+docker build -f ./build_scripts/Dockerfile_cpu_local . -t leynse/sfincs-cpu > build.log 2>&1
 docker push leynse/sfincs-cpu
