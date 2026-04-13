f380014f50cf8dc1fe2d475f9682d18e5d9141ad
Leynse <Tim.Leijnse@deltares.nl>
2026-04-13 09:37:29 +0200

    - Add input variable 'snapwave_waveforces_ratio' which you can set to 0 to turn off wave forces and thus incident wave setup

--- source/src/sfincs_ncoutput.F90
+++ source/src/sfincs_ncoutput.F90
@@ -4211,7 +4211,7 @@ contains
         NF90(nf90_put_att(ncid, varid, 'snapwave_nrsweeps',nr_sweeps)) 
         NF90(nf90_put_att(ncid, varid, 'snapwave_baldock_exponent',baldock_exponent)) 
         NF90(nf90_put_att(ncid, varid, 'snapwave_baldock_ratio',baldock_ratio)) 
-        NF90(nf90_put_att(ncid, varid, 'snapwave_waveforces_factor',waveforces_factor))
+        NF90(nf90_put_att(ncid, varid, 'snapwave_waveforces_ratio',waveforces_ratio))
         !
         ! SnapWave IG
         !
