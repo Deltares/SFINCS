a5dc6314fcd5bcc28528a62b60390a727c81c788
Leynse <Tim.Leijnse@deltares.nl>
2026-04-14 15:36:12 +0200

    - Update names

--- source/src/sfincs_ncoutput.F90
+++ source/src/sfincs_ncoutput.F90
@@ -1076,28 +1076,28 @@ contains
    !
    if (store_vegetation) then
       !
-      NF90(nf90_def_var(map_file%ncid, 'snapwave_veg_Cd', NF90_FLOAT, (/map_file%nmesh2d_face_dimid, map_file%nsec_dimid/), map_file%veg_cd_varid))
+      NF90(nf90_def_var(map_file%ncid, 'vegetation_cd', NF90_FLOAT, (/map_file%nmesh2d_face_dimid, map_file%nsec_dimid/), map_file%veg_cd_varid))
       NF90(nf90_def_var_deflate(map_file%ncid, map_file%veg_cd_varid, 1, 1, nc_deflate_level))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_cd_varid, '_FillValue', FILL_VALUE))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_cd_varid, 'units', '-'))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_cd_varid, 'standard_name', 'vegetation_cd'))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_cd_varid, 'long_name', 'bulk_drag_coefficient_per_vegetation_section'))
       !
-      NF90(nf90_def_var(map_file%ncid, 'snapwave_veg_ah', NF90_FLOAT, (/map_file%nmesh2d_face_dimid, map_file%nsec_dimid/), map_file%veg_ah_varid))
+      NF90(nf90_def_var(map_file%ncid, 'vegetation_stems_height', NF90_FLOAT, (/map_file%nmesh2d_face_dimid, map_file%nsec_dimid/), map_file%veg_ah_varid))
       NF90(nf90_def_var_deflate(map_file%ncid, map_file%veg_ah_varid, 1, 1, nc_deflate_level))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_ah_varid, '_FillValue', FILL_VALUE))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_ah_varid, 'units', 'm'))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_ah_varid, 'standard_name', 'vegetation_stems_height'))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_ah_varid, 'long_name', 'vegetation_section_thickness'))
       !
-      NF90(nf90_def_var(map_file%ncid, 'snapwave_veg_bstems', NF90_FLOAT, (/map_file%nmesh2d_face_dimid, map_file%nsec_dimid/), map_file%veg_bstems_varid))
+      NF90(nf90_def_var(map_file%ncid, 'vegetation_stems_width', NF90_FLOAT, (/map_file%nmesh2d_face_dimid, map_file%nsec_dimid/), map_file%veg_bstems_varid))
       NF90(nf90_def_var_deflate(map_file%ncid, map_file%veg_bstems_varid, 1, 1, nc_deflate_level))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_bstems_varid, '_FillValue', FILL_VALUE))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_bstems_varid, 'units', 'm'))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_bstems_varid, 'standard_name', 'vegetation_stems_width'))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_bstems_varid, 'long_name', 'width_of_individual_vegetation_stems_per_section'))
       !
-      NF90(nf90_def_var(map_file%ncid, 'snapwave_veg_Nstems', NF90_FLOAT, (/map_file%nmesh2d_face_dimid, map_file%nsec_dimid/), map_file%veg_Nstems_varid))
+      NF90(nf90_def_var(map_file%ncid, 'vegetation_stems_density', NF90_FLOAT, (/map_file%nmesh2d_face_dimid, map_file%nsec_dimid/), map_file%veg_Nstems_varid))
       NF90(nf90_def_var_deflate(map_file%ncid, map_file%veg_Nstems_varid, 1, 1, nc_deflate_level))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_Nstems_varid, '_FillValue', FILL_VALUE))
       NF90(nf90_put_att(map_file%ncid, map_file%veg_Nstems_varid, 'units', 'm-2'))
