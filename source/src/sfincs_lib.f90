ced76bbf6dbe43c4200b479b3d6bc5febcfe2fe4
Tim Leijnse <tim.leijnse@deltares.nl>
2026-08-14 14:58:23 +0200

    - bump date

--- source/src/sfincs_lib.f90
+++ source/src/sfincs_lib.f90
@@ -95,7 +95,7 @@ module sfincs_lib
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !
    build_revision = "$Rev: v2.4.0 Galibier+branch:wavemaker-two-forcing-sources"
-   build_date     = "$Date: 2026-08-13"
+   build_date     = "$Date: 2026-08-14"
    !
    call write_log('', 1)
    call write_log('------------ Welcome to SFINCS ------------', 1)
